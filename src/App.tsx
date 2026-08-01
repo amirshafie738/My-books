0a9b36f1bd206cda02fb8ed44f77d216229e2ba2
amirshafie <amirshafie738@gmail.comgit config --global user.name amirshafiegit config --global user.email amirshafie738@gmail.com>
2026-08-01 18:20:49 +0330

    feat:creat book list component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import BookForm from "./component/BookForm";
 import Header from "./component/Header";
 import { reducer } from "./reducer/reducer";
 import { initialState } from "./reducer/initialstate";
+import BookList from "./component/BookList";
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -14,11 +15,13 @@ function App() {
       <main className="max-w-7xl mx-auto p-6">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
           <div className="bg-white rounded-xl shadow p-6">
-            <BookForm />
+            <BookForm dispatch={dispatch} />
           </div>
 
           <div className="bg-white rounded-xl shadow p-6 lg:col-span-2">
-            Books
+            <h2 className="text-2xl font-bold mb-6">Books</h2>
+
+            <BookList books={state.books} />
           </div>
         </div>
       </main>
--- src/validation/bookSchema.ts
+++ src/validation/bookSchema.ts
@@ -1,12 +1,20 @@
-import z from "zod";
+import { z } from "zod";
 
 export const bookSchema = z.object({
-  title: z.string().min(3, "Book title is requred"),
+  title: z.string().min(3, "Book title is required"),
   author: z.string().min(3, "Author is required"),
-  rate: z.coerce
+
+  rate: z
     .number()
     .min(1, "Minimum rate is 1")
     .max(5, "Maximum rate is 5"),
+
   image: z.string().url("Enter a valid image url"),
 });
-export type BookFormData =z.infer<typeof bookSchema>;
+
+export type BookFormData = {
+  title: string;
+  author: string;
+  rate: number;
+  image: string;
+};
