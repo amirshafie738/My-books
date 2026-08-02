61d30b98763d8c069367acf52134be69b6e8ab11
amirshafie <amirshafie738@gmail.comgit config --global user.name amirshafiegit config --global user.email amirshafie738@gmail.com>
2026-08-02 20:39:18 +0330

    feat: add delete book functionality

--- src/component/BookList.tsx
+++ src/component/BookList.tsx
@@ -1,10 +1,23 @@
 import type { Book } from "../types/book";
+import type { Dispatch } from "react";
+import type { Action } from "../reducer/actions";
+import { deleteBook } from "../api/bookApis";
 
 interface Props {
   books: Book[];
+  dispatch: Dispatch<Action>;
 }
 
-function BookList({ books }: Props) {
+function BookList({ books, dispatch }: Props) {
+  const handleDelete = async (id: string) => {
+    try {
+      await deleteBook(id);
+      dispatch({ type: "DELETE_BOOK", payload: id });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   if (books.length === 0) {
     return (
       <div className="h-[500px] flex items-center justify-center border-2 border-dashed rounded-lg">
@@ -16,27 +29,23 @@ function BookList({ books }: Props) {
   return (
     <div className="space-y-4">
       {books.map((book) => (
-        <div
-          key={book.id}
-          className="border rounded-lg p-4 shadow-sm"
-        >
-          <h3 className="text-xl font-semibold">
-            {book.title}
-          </h3>
-
-          <p className="text-slate-600">
-            Author: {book.author}
-          </p>
-
-          <p className="text-yellow-500">
-            ⭐ {book.rate}
-          </p>
+        <div key={book.id} className="border rounded-lg p-4 shadow-sm">
+          <h3 className="text-xl font-semibold">{book.title}</h3>
+          <p className="text-slate-600">Author: {book.author}</p>
+          <p className="text-yellow-500">⭐ {book.rate}</p>
 
           <img
             src={book.image}
             alt={book.title}
             className="mt-3 h-48 object-cover rounded"
           />
+
+          <button
+            onClick={() => handleDelete(book.id)}
+            className="mt-3 rounded-lg border border-red-400 px-4 py-2 text-sm font-medium text-red-500 transition hover:bg-red-50"
+          >
+            Delete
+          </button>
         </div>
       ))}
     </div>
